d6fa7a8a93f540ab8aeb1ba5011bc9e6e6702dcb
pr1amos <157016419+pr1amos@users.noreply.github.com>
2024-07-10 10:45:19 +0300

    Add files via upload

---
+++ Unpacking.py
@@ -0,0 +1,22 @@
+def print_params(a=1, b='строка', c=True):
+   print(a, b, c)
+
+
+print_params()
+print_params(10)
+print_params(10, 'новая строка')
+print_params(10, 'новая строка', False)
+
+
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+
+values_list = [10, 'список', False]
+values_dict = {'a': 20, 'b': 'словарь', 'c': True}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
